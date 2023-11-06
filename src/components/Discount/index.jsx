535c25f173fe62e4eb9c2e8415724fbb03287a48
Dmitriy.Tokarev <kerlon1301@gmail.com>
2023-11-06 19:11:55 +0100

    modified to receive a discount and send an order

--- src/components/Discount/index.jsx
+++ src/components/Discount/index.jsx
@@ -1,13 +1,36 @@
 import styles from './index.module.css';
 import discountImg from '../../utils';
-import { PhoneInput } from 'react-international-phone';
+import { useForm } from 'react-hook-form';
+import { sendSale } from '../../core/redux/store/slices/saleSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import InputMask from 'react-input-mask';
 import { useState } from 'react';
-import 'react-international-phone/style.css';
-import { Button } from '@mui/material';
+import { resetSaleStatus } from '../../core/redux/store/slices/saleSlice';
+import { useEffect } from 'react';
+import { Box, Button, Modal, Typography } from '@mui/material';
 
 function Discount ({saleRef}) {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({mode:"onChange"});
+    const dispatch = useDispatch();
+    const [phone, setPhone] = useState(null);
+    const saleStatus = useSelector((state) => state.sale.status);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const [phone, setPhone] = useState('');
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        dispatch(resetSaleStatus());
+    };
+    const onSubmit = (data) => {
+        dispatch(sendSale(data))
+        setPhone("");
+    };
+
+    useEffect(() => {
+        if (saleStatus === 'resolved') {
+          setIsModalOpen(true);
+          setPhone(null);
+        }
+    }, [saleStatus]);
 
     return (
         <section className={styles.discount}>
@@ -18,25 +41,45 @@ function Discount ({saleRef}) {
                 <div>
                     <h3 className={styles.discount_title}>5% off</h3>
                     <p className={styles.discount_text}>on the first order</p>
-                    <PhoneInput
-                        className={styles.phone_input}
-                        defaultCountry="ua"
-                        value={phone}
-                        onChange={(phone) => setPhone(phone)}
-                    />
-                    <Button 
-                        style={{color: "white", 
-                            backgroundColor: "green", 
-                            border: "1px solid white",
-                            width:"473px",
-                            marginTop:"30px"
-                            }}
-                        variant='contained'
-                    >
-                        Get a discount
-                    </Button>
+                    <form className={styles.registration_form} onSubmit={handleSubmit(onSubmit)}>
+                        <div>
+                            <InputMask
+                                value={phone}
+                                onChange={(event) => setPhone(event.target.value)}
+                                mask="+4 (999) 999-9999"
+                                maskChar="_" 
+                                {...register('phone', {
+                                    required: 'This field is required',
+                                    pattern: {
+                                        value: /^\+\d{1,3}\s\(\d{3}\)\s\d{3}-\d{4}$/,
+                                        message: 'Please enter a valid phone number'
+                                    },
+                                })} 
+                                className={styles.phone_input}
+                            />
+                            {errors.phone && <p className={styles.error_message}>{errors.phone.message}</p>}
+                        </div>
+                        <button type='submit' className={styles.submit_button}>Get a discount</button>
+                    </form>
                 </div>
             </div>
+            <Modal open={isModalOpen} onClose={handleCloseModal} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <Box p={3} bgcolor="white" borderRadius={4}>
+                <Typography variant="h5" gutterBottom>
+                    Discount Confirmation
+                </Typography>
+                <Typography variant="body1">
+                    Discount request has been sent successfully!
+                </Typography>
+                <Button 
+                    variant="contained" 
+                    onClick={handleCloseModal}
+                    style={{background: '#393', borderRadius: '5px', marginTop:"25px", width: "100%"}} 
+                >
+                    Close
+                </Button>
+                </Box>
+            </Modal> 
         </section>
     )
 };
--- src/components/OrderDetails/index.jsx
+++ src/components/OrderDetails/index.jsx
@@ -1,28 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './index.module.css';
 import { calculateOrderTotal } from '../../utils';
-import { Box, Button,CircularProgress,Modal,TextField, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { Box, Button,CircularProgress,Modal, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { sendOrder, resetOrderStatus } from '../../core/redux/store/slices/orderSlice';
 import { resetCart } from '../../core/redux/store/slices/cartSlice';
+import InputMask from 'react-input-mask';
 
 function OrderDetails () {
     const cartItems = useSelector((state) => state.cart);
-    const [phoneNumber, setPhoneNumber] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState(null);
     const dispatch = useDispatch();
     const orderStatus = useSelector((state) => state.order.status);
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({mode:"onChange"});
 
     const handlePhoneChange = (event) => {
         setPhoneNumber(event.target.value); 
     };
-
-    const handleOrderSend = () => {
-        const orderData = {
-          phone: phoneNumber,
-          items: cartItems, 
-        };
-        dispatch(sendOrder(orderData));
-    }
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -32,9 +27,21 @@ function OrderDetails () {
         dispatch(resetCart());
     };
 
+    const onSubmit = (data) => {
+        if(cartItems.length !== 0){
+            const orderData = {
+                phone: phoneNumber,
+                items: cartItems, 
+              };
+            dispatch(sendOrder(orderData));
+            setPhoneNumber("");
+        } 
+    };
+
     useEffect(() => {
         if (orderStatus === 'resolved') {
           setIsModalOpen(true);
+          setPhoneNumber(null);
         }
     }, [orderStatus]);
     
@@ -45,25 +52,34 @@ function OrderDetails () {
                 <p className={styles.total_text}>Total</p>
                 <p className={styles.total_amount}>{calculateOrderTotal(cartItems)}</p>
             </div>
-            <TextField
-                label="Phone number"
-                fullWidth
-                variant="outlined"
-                value={phoneNumber}
-                onChange={handlePhoneChange}
-            />
-            {orderStatus === 'loading' ? (
-            <CircularProgress color="success" style={{marginTop:"25px"}}/> 
-            ) : (
-            <Button
-                style={{background: '#393', borderRadius: '5px', marginTop:"25px", width: "100%"}} 
-                variant='contained'
-                onClick={handleOrderSend}
-            >
-                Order
-            </Button>
-            )}
-
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <InputMask
+                    value={phoneNumber}
+                    onChange={(event) => setPhoneNumber(event.target.value)}
+                    mask="+4 (999) 999-9999"
+                    maskChar="_"
+                    {...register('phone', {
+                        required: 'This field is required',
+                        pattern: {
+                            value: /^\+\d{1,3}\s\(\d{3}\)\s\d{3}-\d{4}$/,
+                            message: 'Please enter a valid phone number'
+                        },
+                    })}
+                    className={styles.phone_input}
+                />
+                {errors.phone && <p className={styles.error_message}>{errors.phone.message}</p>}
+                {orderStatus === 'loading' ? (
+                <CircularProgress color="success" style={{marginTop:"25px"}}/> 
+                ) : (
+                <Button
+                    type='submit'
+                    style={{background: '#393', borderRadius: '5px', marginTop:"25px", width: "100%", height: "75px", borderRadius :"17px", fontSize: "28px"}} 
+                    variant='contained'
+                >
+                    Order
+                </Button>
+                )}
+            </form>
             <Modal open={isModalOpen} onClose={handleCloseModal} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <Box p={3} bgcolor="white" borderRadius={4}>
                 <Typography variant="h5" gutterBottom>
